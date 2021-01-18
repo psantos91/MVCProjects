2f05ba2f1d22ae6505775f8f969781c181270c4e
Paulo Santos <pauloalsantos91@outlook.com>
2021-01-18 14:41:40 +0000

    SellerFormViewModel

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -4,36 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
 {
     public class SellersController : Controller
     {
-        private readonly SellerService _sellerservice;
+        private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
-            _sellerservice = sellerService;
+            _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
         {
-            var list = _sellerservice.FindAll();
+            var list = _sellerService.FindAll();
 
             return View(list);
         }
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
-            _sellerservice.Insert(seller);
+            _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
     }
